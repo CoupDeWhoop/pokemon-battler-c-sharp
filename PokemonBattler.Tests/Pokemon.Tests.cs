d93535b9d5eb602e805ee9c4c97df45acdcf4c1b
CoupDeWhoop <greenland.david@gmail.com>
2024-09-03 09:31:31 +0100

    derived classes fields and methods tested

--- PokemonBattler.Tests/Pokemon.Tests.cs
+++ PokemonBattler.Tests/Pokemon.Tests.cs
@@ -68,14 +68,209 @@ public class PokemonTests
         Assert.That(hasFainted, Is.EqualTo(false));
     }    
 
-    [Test]
-    public void HasFainted_returnsTrue_IfHitpointsZero()
+    [TestCase(80)]
+    [TestCase(100)]
+    public void HasFainted_returnsTrue_IfHitpointsZero(int damage)
     {
         var pokemon = new Pokemon("Pikachu", 80, 20, "Thunderbolt");
 
-        pokemon.TakeDamage(80);
+        pokemon.TakeDamage(damage);
         var hasFainted = pokemon.HasFainted();
 
         Assert.That(hasFainted, Is.EqualTo(true));
-    }       
+    }
+}
+
+public class FireTests
+{
+
+    private Fire _fire;
+
+    [SetUp]
+    public void Setup()
+    {
+        _fire = new Fire("Firemon", 80, 20);
+    }
+
+    [Test]
+    public void IsEffectiveAgainst_returnsCorrectly_whenStrongToOpponent()
+    {
+        var grassmon = new Grass("Grassmon", 80, 20);
+
+        var effective = _fire.IsEffectiveAgainst(grassmon);
+
+        Assert.That(effective, Is.EqualTo(true));
+    }      
+    
+    [Test]
+    public void IsEffectiveAgainst_returnsCorrectly_whenNotStrongToOpponent()
+    {
+        var normon = new Normal("Normon", 80, 20);
+
+        var effective = _fire.IsEffectiveAgainst(normon);
+
+        Assert.That(effective, Is.EqualTo(false));
+    }  
+
+    [Test]
+    public void IsWeakTo_returnsCorrectly_whenWeakToOpponent()
+    {
+        var water = new Water("Watermon", 80, 20);
+
+        var weakTo = _fire.IsWeakTo(water);
+
+        Assert.That(weakTo, Is.EqualTo(true));
+    } 
+
+    [Test]
+    public void IsWeakTo_returnsCorrectly_whenNotWeakToOpponent()
+    {
+        var normal = new Normal("Normon", 80, 20);
+
+        var weakTo = _fire.IsWeakTo(normal);
+
+        Assert.That(weakTo, Is.EqualTo(false));
+    } 
+}
+
+public class GrassTests
+{
+
+    private Grass _grass;
+
+    [SetUp]
+    public void Setup()
+    {
+        _grass = new Grass("Grassmon", 80, 20);
+    }
+
+    [Test]
+    public void IsEffectiveAgainst_returnsCorrectly_whenStrongToOpponent()
+    {
+        var water = new Water("Grassmon", 80, 20);
+
+        var effective = _grass.IsEffectiveAgainst(water);
+
+        Assert.That(effective, Is.EqualTo(true));
+    }      
+    
+    [Test]
+    public void IsEffectiveAgainst_returnsCorrectly_whenNotStrongToOpponent()
+    {
+        var normal = new Normal("Normon", 80, 20);
+
+        var effective = _grass.IsEffectiveAgainst(normal);
+
+        Assert.That(effective, Is.EqualTo(false));
+    }  
+
+    [Test]
+    public void IsWeakTo_returnsCorrectly_whenWeakToOpponent()
+    {
+        var fire = new Fire("Firemon", 80, 20);
+
+        var weakTo = _grass.IsWeakTo(fire);
+
+        Assert.That(weakTo, Is.EqualTo(true));
+    } 
+
+    [Test]
+    public void IsWeakTo_returnsCorrectly_whenNotWeakToOpponent()
+    {
+        var normal = new Normal("Normon", 80, 20);
+
+        var weakTo = _grass.IsWeakTo(normal);
+
+        Assert.That(weakTo, Is.EqualTo(false));
+    } 
+}
+
+public class WaterTests
+{
+
+    private Water _water;
+
+    [SetUp]
+    public void Setup()
+    {
+        _water = new Water("Watermon", 80, 20);
+    }
+
+    [Test]
+    public void IsEffectiveAgainst_returnsCorrectly_whenStrongToOpponent()
+    {
+        var fire = new Fire("Firemon", 80, 20);
+
+        var effective = _water.IsEffectiveAgainst(fire);
+
+        Assert.That(effective, Is.EqualTo(true));
+    }      
+    
+    [Test]
+    public void IsEffectiveAgainst_returnsCorrectly_whenNotStrongToOpponent()
+    {
+        var grass = new Grass("Grassmon", 80, 20);
+
+        var effective = _water.IsEffectiveAgainst(grass);
+
+        Assert.That(effective, Is.EqualTo(false));
+    }  
+
+    [Test]
+    public void IsWeakTo_returnsCorrectly_whenWeakToOpponent()
+    {
+        var grass = new Grass("Grassmon", 80, 20);
+
+        var weakTo = _water.IsWeakTo(grass);
+
+        Assert.That(weakTo, Is.EqualTo(true));
+    } 
+
+    [TestCase()]
+    public void IsWeakTo_returnsCorrectly_whenNotWeakToOpponent()
+    {
+        var fire = new Fire("Firemon", 80, 20);
+        var normal =  new Normal("Normon", 80, 20);
+
+        var weakToFire = _water.IsWeakTo(fire);
+        var weakToNormal = _water.IsWeakTo(normal);
+
+        Assert.That(weakToFire, Is.EqualTo(false));
+        Assert.That(weakToNormal, Is.EqualTo(false));
+    } 
+}
+
+public class NormalTests
+{
+
+    private Normal _normal;
+
+    [SetUp]
+    public void Setup()
+    {
+        _normal = new Normal("Normon", 80, 20);
+    }    
+    
+    [Test]
+    public void IsEffectiveAgainst_returnsCorrectly_whenNotStrongToOpponent()
+    {
+        var grass = new Grass("Grassmon", 80, 20);
+
+        var effective = _normal.IsEffectiveAgainst(grass);
+
+        Assert.That(effective, Is.EqualTo(false));
+    }  
+
+    [TestCase()]
+    public void IsWeakTo_returnsCorrectly_whenNotWeakToOpponent()
+    {
+        var fire = new Fire("Firemon", 80, 20);
+        var normal =  new Normal("Normon", 80, 20);
+
+        var weakToFire = _normal.IsWeakTo(fire);
+        var weakToNormal = _normal.IsWeakTo(normal);
+
+        Assert.That(weakToFire, Is.EqualTo(false));
+        Assert.That(weakToNormal, Is.EqualTo(false));
+    } 
 }
--- PokemonBattler/Pokemon.cs
+++ PokemonBattler/Pokemon.cs
@@ -6,6 +6,8 @@ public class Pokemon
     public int HitPoints;
     public int AttackDamage;
     public string Move;
+    public virtual string Type { get; } = "normal";
+
 
     public Pokemon(string name, int hitPoints, int attackDamage, string move = "tackle")
     {
@@ -28,4 +30,76 @@ public class Pokemon
 
     public bool HasFainted() => HitPoints <= 0;
     
+    public virtual bool IsEffectiveAgainst(Pokemon pokemon)
+    {
+        return false;
+    }
+
+    public virtual bool IsWeakTo(Pokemon pokemon)
+    {
+        return false;
+    }
+}
+
+public class Fire : Pokemon
+{
+    public override string Type { get;} = "fire";
+
+    public Fire(string name, int hitPoints, int attackDamage, string move = "tackle") 
+        : base(name, hitPoints, attackDamage, move){ }
+
+    public override bool IsEffectiveAgainst(Pokemon pokemon)
+    {
+        return pokemon.Type == "grass";
+    }
+
+    public override bool IsWeakTo(Pokemon pokemon)
+    {
+        return pokemon.Type == "water";
+    }
+}
+
+public class Grass : Pokemon
+{
+    public override string Type { get;} = "grass";
+
+    public Grass(string name, int hitPoints, int attackDamage, string move = "tackle") 
+        : base(name, hitPoints, attackDamage, move){ }
+
+    public override bool IsEffectiveAgainst(Pokemon pokemon)
+    {
+        return pokemon.Type == "water";
+    }
+
+    public override bool IsWeakTo(Pokemon pokemon)
+    {
+        return pokemon.Type == "fire";
+    }
+}
+
+public class Water : Pokemon
+{
+    public override string Type { get;} = "water";
+
+    public Water(string name, int hitPoints, int attackDamage, string move = "tackle") 
+        : base(name, hitPoints, attackDamage, move){ }
+
+    public override bool IsEffectiveAgainst(Pokemon pokemon)
+    {
+        return pokemon.Type == "fire";
+    }
+
+    public override bool IsWeakTo(Pokemon pokemon)
+    {
+        return pokemon.Type == "grass";
+    }
+}
+
+public class Normal : Pokemon
+{
+    public override string Type { get;} = "normal";
+
+    public Normal(string name, int hitPoints, int attackDamage, string move = "tackle") 
+        : base(name, hitPoints, attackDamage, move){ }
+
 }
